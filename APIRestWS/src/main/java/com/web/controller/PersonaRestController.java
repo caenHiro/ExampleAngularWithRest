263fcd54a997f3367e0de87fa7b62255bb0db215
caenHiro <caen@ciencias.unam.mx>
2021-04-28 17:31:10 -0500

    se agrega notaciones para el cross origin

--- APIRestWS/src/main/java/com/web/controller/PersonaRestController.java
+++ APIRestWS/src/main/java/com/web/controller/PersonaRestController.java
@@ -3,6 +3,7 @@ package com.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,23 +24,26 @@ public class PersonaRestController {
 	private IPersonaService service;
 	
 	@SuppressWarnings("unchecked")
+	@CrossOrigin
 	@GetMapping("/listarJson")
     public List<Persona> listarJson(  ) {
 
         return   (List<Persona>) service.regresaPersonas();
     }
 	
+	@CrossOrigin
 	@PostMapping("/agregar")
 	public Persona inserta (@RequestBody Persona per) {
 		return service.agregaPersona(per);
 	}
 	
+	@CrossOrigin
 	@PutMapping("/modificar")
 	public Persona modificar (@RequestBody Persona per) {
 		return service.modificarPersona(per);
 	}
 	
-	
+	@CrossOrigin
 	@DeleteMapping( value = "/{id}")
 	public void eliminar(@PathVariable("id")  Integer id) {
 		service.eliminarPersona(id);
